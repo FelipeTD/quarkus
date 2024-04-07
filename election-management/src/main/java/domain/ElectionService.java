fd0b7a1108fdaed4c19933aa5d81de4314a977a5
filipe.dias <fedispato@gmail.com>
2024-04-07 18:28:12 -0300

    feat: adicionando parte de iniciar uma eleicao utilizando redis e banco de dados

---
+++ election-management/src/main/java/domain/ElectionService.java
@@ -0,0 +1,22 @@
+package domain;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Instance;
+
+@ApplicationScoped
+public class ElectionService {
+
+    private final Instance<ElectionRepository> repositories;
+    private final CandidateService candidateService;
+
+    public ElectionService(CandidateService candidateService, Instance<ElectionRepository> repositories) {
+        this.candidateService = candidateService;
+        this.repositories = repositories;
+    }
+
+    public void submit() {
+        Election election = Election.create(candidateService.findAll());
+        repositories.forEach(repository -> repository.submit(election));
+    }
+
+}
